ad80e7e5966f346d1f37dff718022e5a409d4cdc
Rana Marwat Hussain <rana.hussain@hpi.de>
2024-02-11 15:15:52 +0100

    Added simple comment for the reader

--- dbcon.php
+++ dbcon.php
@@ -9,4 +9,5 @@
 	if($conn->connect_error){
 		echo "Seems like you have not configured the database. Failed To Connect to database:" . $conn->connect_error;
 	}
+	//updated using local repository updation by vs code 
 ?>
